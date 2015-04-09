9bed10c73f49db0f73e5bcf9f1a11769e7d96713
Abeer Khakwani <abeerkhakwani@hotmail.com>
2015-04-08 20:04:13 -0700

    Pass second and third  spec

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,29 @@ var triangleChecker = function(side1, side2, side3)
     var second2Added = side2 + side3; //2+8 =10
     var lastAndFirstAdded = side3 + side1; //2+8=10
     var returnString = "";
+
     if ((first2Added <= side3) || (lastAndFirstAdded <= side2) || (second2Added <= side1))
     {
         returnString += "Please enter sides that make a triangle";
     }
     else
-    {}
+    {
+      if((side1===side2)&&(side1===side3)){
+
+          returnString+="This is an equilateral";
+      } 
+      else if((side1===side2)|| (side1===side3)||(side2===side3))
+      {
+
+        returnString+="This is an isosceles";
+
+      }
+      else{
+           returnString+="This is a scalene";
+
+      }
+    }
+
+
     return returnString;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -11,9 +11,19 @@ describe('triangleChecker', function() {
          it("checks to see if a triangle can even be formed with the three sides", function() {
         expect(triangleChecker(8,2,10)).to.equal("Please enter sides that make a triangle");
     });
-          it("checks to see if a triangle can even be formed with the three sides", function() {
-        expect(triangleChecker(2,2,3)).to.equal("Please enter sides that make a triangle");
+          it("checks to see if a triangle is an equalatrial Triangle", function() {
+        expect(triangleChecker(2,2,2)).to.equal("This is an equilateral");
     });
+        it("checks to see if a triangle is an isoceles Triangle", function() {
+        expect(triangleChecker(2,2,3)).to.equal("This is an isosceles");
+    });
+        it("checks to see if a triangle is an isoceles Triangle", function() {
+        expect(triangleChecker(2,3,2)).to.equal("This is an isosceles");
+    });
+        it("checks to see if a triangle is an isoceles Triangle", function() {
+        expect(triangleChecker(3,2,2)).to.equal("This is an isosceles");
+    });
+
 
 
   
