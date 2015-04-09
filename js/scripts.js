32767376b1429ef82c90b6af61104df87638f7a0
Abeer Khakwani <abeerkhakwani@hotmail.com>
2015-04-08 19:52:57 -0700

    Pass first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,14 @@
-var Scrabble = function(letter) {
-
-    letter = letter.toLowerCase();
-    //console.log("lettr= ", letter)
-    var letterArray = letter.split("");
-
-    //console.log("letterArray= ", letterArray)
-
-    var point = {
-        a: 1,e: 1,i: 1, o: 1, u: 1, l:1,n: 1,r: 1,s: 1,t: 1,d: 2,
-        g: 2,
-        b: 3,c: 3,m: 3,p: 3,
-        f: 4,h: 4,v: 4, w: 4,y: 4,
-        k: 5,j: 8,x: 8,
-        q: 10,z: 10
-    };
-    var returnNumber = 0;
-
-    //Foreach function loops through the letterArray and 
-    letterArray.forEach(function(word, index) {
-        var number = point[word];
-
-        number = Math.floor(number);
-        console.log("number= ", number)
-
-
-        returnNumber += number;
-    });
-
-
-    return returnNumber;
-
-
+var triangleChecker = function(side1, side2, side3)
+{
+    var first2Added = side1 + side2; // 2+2=4
+    var second2Added = side2 + side3; //2+8 =10
+    var lastAndFirstAdded = side3 + side1; //2+8=10
+    var returnString = "";
+    if ((first2Added <= side3) || (lastAndFirstAdded <= side2) || (second2Added <= side1))
+    {
+        returnString += "Please enter sides that make a triangle";
+    }
+    else
+    {}
+    return returnString;
 }
-
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,21 @@
-describe('Scrabble', function() {
+describe('triangleChecker', function() {
     it("checks to see if a triangle can even be formed with the three sides", function() {
-        expect(Scrabble(2,2,8)).to.equal("Please enter sides that make a triangle");
+        expect(triangleChecker(2,2,8)).to.equal("Please enter sides that make a triangle");
     });
+      it("checks to see if a triangle can even be formed with the three sides", function() {
+        expect(triangleChecker(2,8,2)).to.equal("Please enter sides that make a triangle");
+    });
+        it("checks to see if a triangle can even be formed with the three sides", function() {
+        expect(triangleChecker(8,2,2)).to.equal("Please enter sides that make a triangle");
+    });
+         it("checks to see if a triangle can even be formed with the three sides", function() {
+        expect(triangleChecker(8,2,10)).to.equal("Please enter sides that make a triangle");
+    });
+          it("checks to see if a triangle can even be formed with the three sides", function() {
+        expect(triangleChecker(2,2,3)).to.equal("Please enter sides that make a triangle");
+    });
+
+
   
     
 });
