7f501ded263c69054143cf9992bafdf773cdaa69
Abeer Khakwani <abeerkhakwani@hotmail.com>
2015-04-10 09:30:11 -0700

    Test and make all types of triangles pass through the interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 var triangleChecker = function(side1, side2, side3)
 {
+
+    side1 = parseInt(side1);
+    side2 = parseInt(side2);
+    side3 = parseInt(side3);
     var first2Added = side1 + side2; // 2+2=4
     var second2Added = side2 + side3; //2+8 =10
     var lastAndFirstAdded = side3 + side1; //2+8=10
@@ -34,11 +38,11 @@ var triangleChecker = function(side1, side2, side3)
 
 $(function(){
   $("form#triangle_form").submit(function(event){
-    var side1= $("input#side1").val();
-    var side2= $("input#side2").val();
-    var side3= $("input#side3").val();
+    var value1 = $("input#side1").val();
+    var value2 = $("input#side2").val();
+    var value3 = $("input#side3").val();
     
-    var result = triangleChecker(side1,side2,side3);
+    var result = triangleChecker(value1,value2,value3);
 
     console.log("result is returning "+ result)
 
